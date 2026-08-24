2446f64e6477277e844f16d1560d3ddd45ef6fe1
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-24 20:17:59 -0300

    in the begginig

---
+++ Calculadora com Operadores Básicos.java
@@ -0,0 +1,5 @@
+// Desenvolva um programa em Java que leia dois números inteiros do usuário 
+// e exiba o resultado das operações básicas (adição, subtração, multiplicação, 
+// divisão e módulo). Utilize Scanner para entrada de dados e trate possíveis 
+// exceções com try/catch.
+
