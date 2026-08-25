a377920a2047e7dd756c66539e084adada756803
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-24 21:31:48 -0300

    dsa

---
+++ Soma_de_Números_com_while.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Soma_de_Números_com_while {
+    public static void main(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("digite um numero inteiro:");
+        int numero = scanner.nextInt();
+    
+        scanner.close();
+    }
+}
