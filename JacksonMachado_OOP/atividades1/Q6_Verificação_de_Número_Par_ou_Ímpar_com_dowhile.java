2c9f585607ad85a2a236803ec69bdc0815dfcd14
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-25 21:44:19 -0300

    doc: ajustando o comentario

--- JacksonMachado_OOP/atividades1/Q6_Verificação_de_Número_Par_ou_Ímpar_com_dowhile.java
+++ JacksonMachado_OOP/atividades1/Q6_Verificação_de_Número_Par_ou_Ímpar_com_dowhile.java
@@ -1,7 +1,7 @@
 package JacksonMachado_OOP.atividades1;
 // Implemente um programa que utilize um laço do/while
-//  para ler números do usuário e verificar se são pares ou ímpares. 
-//  O programa deve continuar até que o usuário digite -1. 
+// para ler números do usuário e verificar se são pares ou ímpares. 
+// O programa deve continuar até que o usuário digite -1. 
 // Utilize o operador módulo (%) e try/catch para validação.
 
 import java.util.Scanner;
