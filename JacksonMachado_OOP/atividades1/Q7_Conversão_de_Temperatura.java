b52147a30b974b67ab9bab7274648c5955601980
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-26 19:19:20 -0300

    feat: arquivo Questao7

---
+++ JacksonMachado_OOP/atividades1/Q7_Conversão_de_Temperatura.java
@@ -0,0 +1,19 @@
+package JacksonMachado_OOP.atividades1;
+
+import java.util.Scanner;
+
+// Desenvolva um programa que converta temperaturas entre Celsius e Fahrenheit. 
+// Ofereça um menu com switch para escolher a conversão desejada. 
+// Utilize Scanner para entrada, 
+// operadores aritméticos para cálculo e try/catch para tratamento de exceções.
+
+public class Q7_Conversão_de_Temperatura {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("digite um numero inteiro:");
+        int numero = scanner.nextInt();
+    
+        scanner.close();
+    }
+}
