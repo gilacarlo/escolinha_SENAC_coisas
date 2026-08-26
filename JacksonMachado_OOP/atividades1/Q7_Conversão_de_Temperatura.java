4be80c7b8944e963ba32edb939bf023daf711fdf
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-26 20:08:30 -0300

    feat: questao 7 feita

--- JacksonMachado_OOP/atividades1/Q7_Conversão_de_Temperatura.java
+++ JacksonMachado_OOP/atividades1/Q7_Conversão_de_Temperatura.java
@@ -11,9 +11,39 @@ public class Q7_Conversão_de_Temperatura {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("digite um numero inteiro:");
-        int numero = scanner.nextInt();
-    
+        boolean sair = false;
+        while (sair == false) {
+            System.out.print("\ndigite uma temperatura:");
+            float temperatura = 0.0f;
+            try{
+                temperatura = scanner.nextFloat();
+            }catch(Exception e){
+                System.out.print("\ndigite algo valido:");
+                scanner.next();
+                continue;
+            }
+
+            System.out.print("\n(1) Celsius para Fahrenheit. "+
+                            "\n(2) Fahrenheit para Celsius");
+            int escolha = 0;
+            try{
+                escolha = scanner.nextInt();
+            }catch(Exception e){
+                System.out.print("\ndigite algo valido:");
+                scanner.next();
+                continue;
+            }
+            if(escolha == 1){
+                temperatura = (temperatura * 1.8f) + 32;
+            }else if(escolha == 2){
+                temperatura = (temperatura - 32) * (5/9);
+            }else{
+                System.out.print("\ndigite algo valido:");
+                continue;
+            }
+            System.out.print("\nTemperatura convertida: " + temperatura);
+        }
+
         scanner.close();
     }
 }
