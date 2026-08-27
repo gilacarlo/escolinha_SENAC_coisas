ac3634f9f690c426a5f50bdcc258580aae2a7e85
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-26 21:56:43 -0300

    feat: Q10 feita

--- JacksonMachado_OOP/atividades1/Q10_Jogo_de_Adivinhação_com_Estruturas_de_Controle.java
+++ JacksonMachado_OOP/atividades1/Q10_Jogo_de_Adivinhação_com_Estruturas_de_Controle.java
@@ -7,14 +7,33 @@ import java.util.Scanner;
 // repetir até acertar, if/else para comparar o palpite com o número secreto, 
 // Scanner para entrada e 
 // try/catch para validação. Exiba dicas ("maior" ou "menor") a cada tentativa.
+import java.util.Random;
 
 public class Q10_Jogo_de_Adivinhação_com_Estruturas_de_Controle {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int randNum = random.nextInt(100);
+        
+        int numero = 0;
+        while (true) {
+            System.out.print("\ndigite um numero inteiro entre 0 e 100:");
+            try{
+                numero = scanner.nextInt();
+            }catch(Exception e){
+                System.out.println("Digite algo válido!");
+            }
 
-        System.out.print("digite um numero inteiro:");
-        int numero = scanner.nextInt();
-    
+            if(numero == randNum){
+                System.out.print("\nvoce ganhou:");
+                break;
+            }else if(numero < randNum){
+                System.out.print("\no numero esta acima desse:");
+            }else if(numero > randNum){
+                System.out.print("\no numero esta abaixo desse:");
+            }
+        }
+
         scanner.close();
     }
 }
