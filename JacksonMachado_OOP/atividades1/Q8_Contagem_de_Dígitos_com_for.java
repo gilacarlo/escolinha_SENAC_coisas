7b37de39d7b90e198c1b566304ee6c519ed7dc3b
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-26 21:32:31 -0300

    feat: q8 feita

--- JacksonMachado_OOP/atividades1/Q8_Contagem_de_Dígitos_com_for.java
+++ JacksonMachado_OOP/atividades1/Q8_Contagem_de_Dígitos_com_for.java
@@ -10,9 +10,21 @@ public class Q8_Contagem_de_Dígitos_com_for {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("digite um numero inteiro:");
-        int numero = scanner.nextInt();
-    
+        int numero = 0;
+        try{
+            System.out.print("digite um numero inteiro e positivo:");
+            numero = scanner.nextInt();
+            if(numero < 0){
+              System.out.print("\ndigite algo valido:");  
+            }else{
+                String texto = Integer.toString(numero);
+                System.out.print("algarismos: " + texto.length());
+            }
+            
+        }catch(Exception e){
+            System.out.print("\ndigite algo valido:");
+            scanner.next();
+        }
         scanner.close();
     }
 }
