51a91a6dbf367af4027db0d71acb1bb194080871
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-31 19:56:58 -0300

    feat: Q1 feita

--- JacksonMachado_OOP/atividades2/Q1_Cadastro_e_Análise_de_Notas_com_Array.java
+++ JacksonMachado_OOP/atividades2/Q1_Cadastro_e_Análise_de_Notas_com_Array.java
@@ -14,9 +14,41 @@ public class Q1_Cadastro_e_Análise_de_Notas_com_Array {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("digite um numero inteiro:");
-        int numero = scanner.nextInt();
-    
+        System.out.print("digite a quatidade de alunos:");
+        int quatAlunos = scanner.nextInt();
+
+        double[] lista = new double[quatAlunos];
+        double maiorNota = -1;
+        double menorNota = 999;
+        double totalNotas = 0;
+        int aprovados = 0;
+
+        for(int i = 0; i < lista.length; i++){
+            System.out.print("digite a nota do alunos:");
+            try{
+                double notaAluno = scanner.nextDouble();
+                lista[i] = notaAluno;
+            }catch(Exception e){
+                System.out.println("Digite algo válido!");
+                scanner.next();
+                lista[i] = 0;
+            }
+            if( lista[i] > maiorNota){
+                maiorNota = lista[i];
+            }
+            if( lista[i] < menorNota){
+                menorNota = lista[i];
+            }
+            if( lista[i] >= 6.0){
+                aprovados++;
+            }
+            totalNotas += lista[i];
+        }
+
+        System.out.print("\nMedia da truma: " + totalNotas / quatAlunos);
+        System.out.print("\nMaior nota: " + maiorNota);
+        System.out.print("\nMenor nota: " + menorNota);
+        System.out.print("\naprovados: " + aprovados);
         scanner.close();
     }
 }
