7f34b3547ca17bc80d0a6b137db25fa2d2ef8ed9
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-08-31 20:25:04 -0300

    feat: Q3 feita

--- JacksonMachado_OOP/atividades2/Q3_SeparaçãodeNúmerosPareseÍmpares.java
+++ JacksonMachado_OOP/atividades2/Q3_SeparaçãodeNúmerosPareseÍmpares.java
@@ -11,10 +11,34 @@ import java.util.Scanner;
 public class Q3_SeparaçãodeNúmerosPareseÍmpares {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        
+        int taman = 15;
+        int[] lista = new int[taman];
 
-        System.out.print("digite um numero inteiro:");
-        int numero = scanner.nextInt();
-    
+        for(int i = 0; i < lista.length; i++){
+            System.out.print("\n" + (i + 1) + ". digite um numero inteiro:");
+            try{
+                int numInt = scanner.nextInt();
+                lista[i] = numInt;
+            }catch(Exception e){
+                System.out.println("Digite algo válido!");
+                scanner.next();
+            }
+        }
+
+        int numerosPares = 0;
+        int numerosimpares = 0;
+        for(int i = 0; i < lista.length; i++){
+            if(lista[i] % 2 == 0){
+                System.out.print("\n" + lista[i] + " esse numero é par.");
+                numerosPares++;
+            }else{
+                System.out.print("\n" + lista[i] + " esse numero é impar.");
+                numerosimpares++;
+            }
+        }
+        System.out.print("\nQuatidade de numeros pares: " + numerosPares );
+        System.out.print("\nQuatidade de numeros impares: " + numerosimpares );
         scanner.close();
     }
 }
