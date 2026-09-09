e4209f4d34d2e3d676fd52ce80ddf7e4ee3e075a
gilacarlo <info.giancarlo2022.02@gmail.com>
2026-09-08 21:34:56 -0300

    fds

--- JacksonMachado_OOP/atividades2/Q7_FiladeAtendimentocomQueue.java
+++ JacksonMachado_OOP/atividades2/Q7_FiladeAtendimentocomQueue.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 // Crie um programa que simule uma fila de atendimento utilizando uma collection 
 // compatível com a interface Queue<String>, como LinkedList<String>. O sistema 
-// deve permitir adicionar um cliente ao final da fila, atender o próximo cliente, 
-// consultar quem será atendido em seguida, listar a fila e encerrar o programa.
+// deve permitir 
+// adicionar um cliente ao final da fila, 
+// atender o próximo cliente, 
+// consultar quem será atendido em seguida, 
+// listar a fila e encerrar o programa.
 // Utilize switch para controlar as opções e do/while para repetir o menu. Antes de 
 // remover ou consultar o próximo cliente, verifique se a fila está vazia para 
 // evitar erros. As entradas devem ser lidas com Scanner e protegidas com try/catch 
